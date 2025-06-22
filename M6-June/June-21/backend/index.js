bf9aa188141a33d43e59a1a2b6bb212308333115
Akash Sharma <soulakash20002@gmail.com>
2025-06-22 18:17:55 +0530

    backend

--- M6-June/June-21/backend/index.js
+++ M6-June/June-21/backend/index.js
@@ -1,21 +1,23 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 // route
-app.get('/route', (req, res) => {
-//   res.send('This is a route response!');
+app.get("/api/route", (req, res) => {
+  //   res.send('This is a route response!');
   const route = [
-    { id: 1, name: 'Route 1' },
-    { id: 2, name: 'Route 2' },
-    { id: 3, name: 'Route 3' }
-  ]
-    res.send(route);
+    { id: 1, title: "Route 1", description: "This is the first route" },
+    { id: 2, title: "Route 2", description: "This is the second route" },
+    { id: 3, title: "Route 3", description: "This is the third route" },
+    { id: 4, title: "Route 4", description: "This is the fourth route" },
+    { id: 5, title: "Route 5", description: "This is the fifth route" },
+  ];
+  res.send(route);
 });
 
 app.listen(PORT, () => {
